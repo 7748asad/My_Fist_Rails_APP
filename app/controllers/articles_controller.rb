2c32e46e551ce829c2fa984fc135fdc2e0cbd9e1
7748asad <asadmehmood7748@gmail.com>
2025-04-04 13:02:20 +0500

    add functions like Crud in it form web

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,39 +23,64 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render "new"
     end
+    # @article = Article.new(article_params)
+
+    # respond_to do |format|
+    #   if @article.save
+    #     format.html { redirect_to @article, notice: "Article was successfully created." }
+    #     format.json { render :show, status: :created, location: @article }
+    #   else
+    #     format.html { render :new, status: :unprocessable_entity }
+    #     format.json { render json: @article.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: "Article was successfully updated." }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render "edit"
     end
+
+
+
+    # respond_to do |format|
+    #   if @article.update(article_params)
+    #     format.html { redirect_to @article, notice: "Article was successfully updated." }
+    #     format.json { render :show, status: :ok, location: @article }
+    #   else
+    #     format.html { render :edit, status: :unprocessable_entity }
+    #     format.json { render json: @article.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # DELETE /articles/1 or /articles/1.json
   def destroy
-    @article.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to articles_path, status: :see_other, notice: "Article was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    # binding.break
+    @article.destroy
+    redirect_to articles_path
   end
+
+
+
+  # @article.destroy!
+
+  # respond_to do |format|
+  #   format.html { redirect_to articles_path, status: :see_other, notice: "Article was successfully destroyed." }
+  #   format.json { head :no_content }
+  # end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
